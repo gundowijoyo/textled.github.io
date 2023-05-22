2032620f59811722a07767c6f5fd961d9d0a84a4
wijoyoCell <133162689+wijoyoCell@users.noreply.github.com>
2023-05-22 18:34:30 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 self.addEventListener('install',function(event){
 //promosie2
 	event.waitUntil(
-	caches.open('cache-ku').then(function(cache){
+	caches.open('cache-ku-v7').then(function(cache){
 	  	return cache.addAll([
 	  		'/index.html',
 	  		'/style.css',
-	  		'/main.js',
+	  		
 	  		'/IMG_20230502_232903-removebg-preview.png'
 	  		])
 	})	
